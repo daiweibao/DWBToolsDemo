450b1c5da06d798916ec496ff1a5f74a42205c5c
戴维保 <497266156@qq.com>
2020-04-14 15:50:30 +0800

    富文本

--- DWBToolsDemo/YYTextViewController.m
+++ DWBToolsDemo/YYTextViewController.m
@@ -10,6 +10,8 @@
 #import <YYText/YYText.h>
 @interface YYTextViewController ()
 @property (nonatomic , strong)   YYLabel *yyLabel;
+@property (nonatomic , strong)   YYLabel *yyLabelXY;
+@property (nonatomic , assign) BOOL isSelectXY;
 @property (nonatomic , assign) BOOL isSelect;
 @end
 
@@ -24,6 +26,10 @@
        _yyLabel.textAlignment = NSTextAlignmentCenter;
        [self.view addSubview:_yyLabel];
        [self protocolIsSelect:NO];
+    
+    
+    self.isSelectXY = NO;
+    [self createYYlabelXYWithSelected:NO];
 }
 - (void)protocolIsSelect:(BOOL)isSelect{
     //设置整段字符串的颜色
@@ -45,10 +51,7 @@
     border.lineStyle = YYTextLineStyleSingle;//边框样式
     border.fillColor = [UIColor orangeColor];//边框里面填充色
     [text yy_setTextBackgroundBorder:border range:[fff1 rangeOfString:fff1Son]];
-  
-//    NSShadow *shadow = [NSShadow new];
-//    shadow.shadowBlurRadius = 5;
-//    [text yy_setShadow:shadow range:[fff1 rangeOfString:fff1Son]];
+
     
     //设置高亮色和点击事件
     [text yy_setTextHighlightRange:[fff1 rangeOfString:@"《用户协议》"] color:[UIColor greenColor] backgroundColor:[UIColor clearColor] tapAction:^(UIView * _Nonnull containerView, NSAttributedString * _Nonnull text, NSRange range, CGRect rect) {
@@ -78,6 +81,86 @@
     //居中显示一定要放在这里，放在viewDidLoad不起作用
     _yyLabel.textAlignment = NSTextAlignmentCenter;
 
+}
+
+#pragma mark ==========创建协议============
+//#import <YYText/YYText.h>
+//@property (nonatomic , strong)   YYLabel *yyLabelXY;
+//@property (nonatomic , assign) BOOL isSelectXY;
+- (void)createYYlabelXYWithSelected:(BOOL )isSelectXY {
+    __weak typeof(self) weakSelf = self;
+    //设置隐登录私协议
+    YYLabel * yyLabel = self.yyLabelXY;
+    if (self.yyLabelXY==nil) {
+        yyLabel = [[YYLabel alloc]init];
+        self.yyLabelXY = yyLabel;
+        [self.view addSubview:yyLabel];
+    }
+    //固定高度
+    yyLabel.frame = CGRectMake(0, 300, SCREEN_WIDTH, 32);
+    yyLabel.textVerticalAlignment = YYTextVerticalAlignmentCenter;
+    yyLabel.textAlignment = NSTextAlignmentLeft;
+    NSString *xyString1 = @"  登录即代表您已同意 ";
+    NSString *xyStrSon2 = @"软件许可及服务协议";
+    NSString *xyStrSon3 = @" 和 ";
+    NSString *xyStrSon4 = @"隐私协议";
+    NSString *xyStrAll = [NSString stringWithFormat:@"%@%@%@%@",xyString1,xyStrSon2,xyStrSon3,xyStrSon4];
+     NSDictionary *attributes = @{NSFontAttributeName:[UIFont systemFontOfSize:11], NSForegroundColorAttributeName:DWBColorHex(@"#808C97")};
+     NSMutableAttributedString *attributedText = [[NSMutableAttributedString alloc] initWithString:xyStrAll attributes:attributes];
+    
+    //(1)设置下划线
+    YYTextDecoration *decoration = [YYTextDecoration decorationWithStyle:YYTextLineStyleSingle width:@(1) color:DWBColorHex(@"#D0D0D0")];
+    [attributedText yy_setTextUnderline:decoration range:[xyStrAll rangeOfString:xyStrSon2]];
+    //设置指定内容字号
+    [attributedText yy_setFont:[UIFont systemFontOfSize:11] range:[xyStrAll rangeOfString:xyStrSon2]];
+    //设置高亮色和点击事件
+    [attributedText yy_setTextHighlightRange:[xyStrAll rangeOfString:xyStrSon2] color:DWBColorHex(@"#D0D0D0") backgroundColor:[UIColor clearColor] tapAction:^(UIView * _Nonnull containerView, NSAttributedString * _Nonnull text, NSRange range, CGRect rect) {
+        DWBAlertShow([text.string substringWithRange:range]);
+        NSLog(@"%@",[text.string substringWithRange:range]);
+       
+        
+    }];
+    //(2)设置下划线
+    YYTextDecoration *decoration2 = [YYTextDecoration decorationWithStyle:YYTextLineStyleSingle width:@(1) color:DWBColorHex(@"#D0D0D0")];
+    [attributedText yy_setTextUnderline:decoration2 range:[xyStrAll rangeOfString:xyStrSon4]];
+    //设置指定内容字号
+    [attributedText yy_setFont:[UIFont systemFontOfSize:11] range:[xyStrAll rangeOfString:xyStrSon4]];
+    //设置高亮色和点击事件
+    [attributedText yy_setTextHighlightRange:[xyStrAll rangeOfString:xyStrSon4] color:DWBColorHex(@"#D0D0D0") backgroundColor:[UIColor clearColor] tapAction:^(UIView * _Nonnull containerView, NSAttributedString * _Nonnull text, NSRange range, CGRect rect) {
+        DWBAlertShow([text.string substringWithRange:range]);
+        NSLog(@"%@",[text.string substringWithRange:range]);
+       
+    }];
+    
+    //(3)添加图片
+    UIImage *image;
+    if (isSelectXY==NO) {
+        image = [UIImage imageNamed:@"unSelectIcon"];
+    }else{
+        image = [UIImage imageNamed:@"selectIcon"];
+    }
+    NSMutableAttributedString *attachment = [NSMutableAttributedString yy_attachmentStringWithContent:image contentMode:UIViewContentModeCenter attachmentSize:CGSizeMake(12, 12) alignToFont:[UIFont fontWithName:@"PingFangSC-Regular"  size:12] alignment:(YYTextVerticalAlignment)YYTextVerticalAlignmentCenter];
+    //将图片放在最前面
+    [attributedText insertAttributedString:attachment atIndex:0];
+    //添加图片的点击事件
+    [attributedText yy_setTextHighlightRange:[[attributedText string] rangeOfString:[attachment string]] color:[UIColor clearColor] backgroundColor:[UIColor clearColor] tapAction:^(UIView * _Nonnull containerView, NSAttributedString * _Nonnull text, NSRange range, CGRect rect) {
+         DWBAlertShow(@"点击了图片")
+        weakSelf.isSelectXY = !weakSelf.isSelectXY;
+        [self createYYlabelXYWithSelected:weakSelf.isSelectXY];
+    }];
+    
+    //赋值
+    yyLabel.attributedText = attributedText;
+    //居中显示一定要放在这里，放在viewDidLoad不起作用
+//    _yyLabel.textAlignment = NSTextAlignmentCenter;
+
+//    //（最后）计算YYLabel文本尺寸-高度
+//    CGSize maxSize = CGSizeMake(SCREEN_WIDTH, MAXFLOAT);
+//    YYTextLayout *layout = [YYTextLayout layoutWithContainerSize:maxSize text:attributedText];
+//    yyLabel.textLayout = layout;
+//    CGFloat introHeight = layout.textBoundingSize.height;
+//     yyLabel.frame = CGRectMake(0, 300, SCREEN_WIDTH, introHeight);
+    
 }
 
 
