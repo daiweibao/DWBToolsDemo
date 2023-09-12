55bc80e34653c913dbe10974bc9e8c9c85224868
daiweibao <daiweibao@csii.com.cn>
2023-09-12 19:17:21 +0800

    xiuagi

--- DWBToolsDemo/DWBTools/Other/CXUUIDHelp.m
+++ DWBToolsDemo/DWBTools/Other/CXUUIDHelp.m
@@ -9,8 +9,8 @@
 #import "CXUUIDHelp.h"
 
 #define  KEY_USERNAME_PASSWORD @"com.company.app.usernamepassword"
-#define  KEY_USERNAME @"com.company.app.username"
-#define  KEY_PASSWORD @"com.company.app.password"
+//#define  KEY_USERNAME @"com.company.app.username"
+//#define  KEY_PASSWORD @"com.company.app.password"
 
 @implementation CXUUIDHelp
 
@@ -24,6 +24,7 @@
             nil];
 }
 
+//将该uuid保存到keychain钥匙串
 + (void)save:(NSString *)service data:(id)data {
     //Get search dictionary
     NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
@@ -36,11 +37,12 @@
 }
 
 
+//读取保存的UUID
 + (id)load:(NSString *)service {
     id ret = nil;
     NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
-    //Configure the search setting
-    //Since in our simple case we are expecting only a single attribute to be returned (the password) we can set the attribute kSecReturnData to kCFBooleanTrue
+    //配置搜索设置
+    //因为在我们简单的例子中，我们只期望返回一个属性(密码)，所以可以将属性kSecReturnData设置为kCFBooleanTrue
     [keychainQuery setObject:(id)kCFBooleanTrue forKey:(id)kSecReturnData];
     [keychainQuery setObject:(id)kSecMatchLimitOne forKey:(id)kSecMatchLimit];
     CFDataRef keyData = NULL;
@@ -57,15 +59,21 @@
     return ret;
 }
 
-+ (void)deleteKeyData:(NSString *)service {
-    NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
-    SecItemDelete((CFDictionaryRef)keychainQuery);
-}
+//删除存储的UUID
+//+ (void)deleteKeyData:(NSString *)service {
+//    NSMutableDictionary *keychainQuery = [self getKeychainQuery:service];
+//    SecItemDelete((CFDictionaryRef)keychainQuery);
+//}
 
 
+/**
+ 得到UUID
+
+ @return 卸载后也不回变的UUID
+ */
 +(NSString *)getUUID
 {
-    NSString * strUUID = (NSString *)[CXUUIDHelp load:@"com.company.app.usernamepassword"];
+    NSString * strUUID = (NSString *)[CXUUIDHelp load:KEY_USERNAME_PASSWORD];
     
     //首次执行该方法时，uuid为空
     if ([strUUID isEqualToString:@""] || !strUUID)
@@ -75,7 +83,7 @@
         
         strUUID = (NSString *)CFBridgingRelease(CFUUIDCreateString (kCFAllocatorDefault,uuidRef));
         
-        //将该uuid保存到keychain
+        //将该uuid保存到keychain钥匙串
         [CXUUIDHelp save:KEY_USERNAME_PASSWORD data:strUUID];
         
     }
@@ -85,3 +93,4 @@
 
 
 @end
+
